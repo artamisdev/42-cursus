9c82447f9f69e0179477b378735e82bbc72f3013
Tamires Campos Braga Rios <tacampos@c2r11s3.42madrid.com>
2023-09-14 20:04:55 +0200

    isalnum a terminar

---
+++ 00-libft/ft_isalnum.c
@@ -0,0 +1,22 @@
+
+static int	ft_isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	return (0);
+}
+
+static int	ft_isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
+
+int isalnum(int c)
+{
+    if (ft_isalpha || ft_isdigit)
+        return (1);
+    return (0);
+}
