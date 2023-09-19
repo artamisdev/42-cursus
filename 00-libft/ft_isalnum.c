48d6f577362f0e1c0941bacc6d38a68fb572b046
Tamires Braga <tamirescamposbraga@gmail.com>
2023-09-19 21:22:38 +0200

    ft_isalnum

--- 00-libft/ft_isalnum.c
+++ 00-libft/ft_isalnum.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+#include <ctype.h>
 
 static int	ft_isdigit(int c)
 {
@@ -14,9 +16,32 @@ static int	ft_isalpha(int c)
 		return (0);
 }
 
-int isalnum(int c)
+int ft_isalnum(int c)
 {
-    if (ft_isalpha || ft_isdigit)
+    if (ft_isalpha(c) || ft_isdigit(c))
         return (1);
     return (0);
+}
+
+int main(void)
+{
+	int i;
+	int j;
+	int	x;
+	int z;
+
+	i = '%';
+	j = '6';
+	x = 'e';
+	z = 'A';
+
+	printf("Result: %d\n", isalnum(i));
+	printf("Result: %d\n", isalnum(j));
+	printf("Result: %d\n", isalnum(x));
+	printf("Result: %d\n", isalnum(z));
+	printf("Result: %d\n", ft_isalnum(i));
+	printf("Result: %d\n", ft_isalnum(j));
+	printf("Result: %d\n", ft_isalnum(x));
+	printf("Result: %d\n", ft_isalnum(z));
+
 }
