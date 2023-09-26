7c8b270d135b0067ac7bbcf11976a524ce67a12c
Tamires Campos Braga Rios <tacampos@c2r14s6.42madrid.com>
2023-09-26 12:18:16 +0200

    memset

--- 00-libft/ft_bzero.c
+++ 00-libft/ft_bzero.c
@@ -1,35 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*   ft_bzero.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tacampos <tacampos@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/26 11:33:31 by tacampos          #+#    #+#             */
-/*   Updated: 2023/09/26 12:13:01 by tacampos         ###   ########.fr       */
+/*   Created: 2023/09/26 12:15:48 by tacampos          #+#    #+#             */
+/*   Updated: 2023/09/26 12:17:50 by tacampos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-/*#include <stdio.h>
-#include <string.h>*/
 
-void	*ft_memset(void *b, int c, size_t len)
-{
-	size_t	i;
-	char	*str;
 
-	i = 0;
-	str = b;
-	while (i < len)
-	{
-		str[i] = c;
-		i++;
-	}
-	return (b);
-}
-
-/*int main(void)
-{
-	char	i[] = "you got this!";
-	
-	printf("Result: %s\n", ft_memset(i, 'H', 6));
-}*/
