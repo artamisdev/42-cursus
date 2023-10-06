3c8cb73660b7e10494107307534f6537852494e8
Tamires Campos Braga Rios <tacampos@c2r4s3.42madrid.com>
2023-10-06 18:35:17 +0200

    Cat

--- 00-libft/ft_strlcat.c
+++ 00-libft/ft_strlcat.c
@@ -6,11 +6,51 @@
 /*   By: tacampos <tacampos@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/26 17:48:44 by tacampos          #+#    #+#             */
-/*   Updated: 2023/10/06 17:17:45 by tacampos         ###   ########.fr       */
+/*   Updated: 2023/10/06 18:33:45 by tacampos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+/*#include <stdio.h>
+#include <string.h>*/
+
+static size_t	ft_strlen(const char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
 
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
+	size_t	i;
+	size_t	dst_len;
+	size_t	src_len;
 
+	dst_len = ft_strlen(dst);
+	src_len = ft_strlen(src);
+	if (dstsize <= dst_len)
+		return (src_len + dstsize);
+	i = 0;
+	while (src[i] && i < dstsize - dst_len - 1)
+	{
+		dst[dst_len + i] = src[i];
+		i++;
+	}
+	dst[dst_len + i] = '\0';
+	return (dst_len + src_len);
 }
+
+/*int main(void)
+{
+	char		dst_a[20] = "Primeira";
+	const char	src_a[] = "&Segunda";
+	char		dst_b[20] = "Primeira";
+	const char	src_b[] = "&Segunda";
+	
+	printf("Result: %zu\n", ft_strlcat(dst_a, src_a, 17));
+	printf("Result: %zu\n", strlcat(dst_b, src_b, 17));
+}*/
