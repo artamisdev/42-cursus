93d249c57fdab4f17ae4a89ef30a6dbe6e54952d
Tamires Braga <tamirescamposbraga@gmail.com>
2023-10-24 19:54:59 +0200

    isalpha teste

--- circle-0/libft/ft_isalpha.c
+++ circle-0/libft/ft_isalpha.c
@@ -6,7 +6,7 @@
 /*   By: tacampos <tacampos@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/13 19:43:18 by tacampos          #+#    #+#             */
-/*   Updated: 2023/10/15 18:51:41 by tacampos         ###   ########.fr       */
+/*   Updated: 2023/10/24 19:52:35 by tacampos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -18,20 +18,3 @@ int	ft_isalpha(int c)
 	else
 		return (0);
 }
-
-/*#include <stdio.h>
-#include <ctype.h>
-
-int	main(void)
-{
-	int	x;
-	int	z;
-
-	x = 'F';
-	z = '3';
-	printf("Result_ft: %d\n", ft_isalpha(x));
-	printf("Result_isalpha: %d\n", isalpha(x));
-	printf("\n");
-	printf("Result_ft: %d\n", ft_isalpha(z));
-	printf("Result_isalpha: %d\n", isalpha(z));
-}*/
