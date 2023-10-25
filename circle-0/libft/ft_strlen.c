2fd4958bc5328952f3c3d51f3f6bf99b47cda078
Tamires Braga <tamirescamposbraga@gmail.com>
2023-10-25 17:29:19 +0200

    strlen teste

--- circle-0/libft/ft_strlen.c
+++ circle-0/libft/ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: tacampos <tacampos@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/13 20:04:47 by tacampos          #+#    #+#             */
-/*   Updated: 2023/10/15 19:22:36 by tacampos         ###   ########.fr       */
+/*   Updated: 2023/10/25 17:25:59 by tacampos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -22,13 +22,3 @@ size_t	ft_strlen(const char *s)
 	}
 	return (i);
 }
-
-/*#include <stdio.h>
-#include <string.h>
-
-int	main(void)
-{
-	printf("Result_ft: %zu\n", ft_strlen("I'm always angry."));
-	printf("Result_strlen: %lu\n", strlen("I'm always angry."));
-	return (0);
-}*/
---
+++ circle-0/libft/tests/ft_strlen.c
@@ -0,0 +1,10 @@
+#include "../libft.h"
+#include <stdio.h>
+#include <string.h>
+
+int	main(void)
+{
+	printf("Result_ft: %zu\n", ft_strlen("I'm always angry."));
+	printf("Result_strlen: %lu\n", strlen("I'm always angry."));
+	return (0);
+}
