cfaca32249b8cdad6aef966e1920db1322f2e777
Tamires Braga <tamirescamposbraga@gmail.com>
2023-10-27 00:06:47 +0200

    strtrim ok norm, teste

--- circle-0/libft/tests/ft_strtrim.c
+++ circle-0/libft/tests/ft_strtrim.c
@@ -1,1 +1,16 @@
 #include "../libft.h"
+#include <stdio.h>
+
+int	main(void)
+{
+	char	*to_set;
+	char	*str;
+	char	*result;
+
+	str = ".,.,.,azul del mar..,,.,";
+	to_set = ".,";
+
+	result = ft_strtrim(str, to_set);
+
+	printf("Result_ft_strtrim: %s\n", result);
+}
