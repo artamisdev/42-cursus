7c69c84c21b0874a4d94dccf89f7e76aac89dd52
Tamires Braga <tamirescamposbraga@gmail.com>
2023-10-28 14:14:36 +0200

    char_set

--- circle-0/libft/tests/ft_strtrim.c
+++ circle-0/libft/tests/ft_strtrim.c
@@ -3,14 +3,14 @@
 
 int	main(void)
 {
-	char	*to_set;
+	char	*char_set;
 	char	*str;
 	char	*result;
 
 	str = ".,.,.,azul del mar..,,.,";
-	to_set = ".,";
+	char_set = ".,";
 
-	result = ft_strtrim(str, to_set);
+	result = ft_strtrim(str, char_set);
 
 	printf("Result_ft_strtrim: %s\n", result);
 }
