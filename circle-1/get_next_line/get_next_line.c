796fa447b6c3368538a742e94437d5cf8d4273c9
Tamires Campos Braga Rios <tacampos@c1r10s6.42madrid.com>
2024-06-07 14:19:42 +0200

    no comments

--- circle-1/get_next_line/get_next_line.c
+++ circle-1/get_next_line/get_next_line.c
@@ -2,27 +2,27 @@
 
 static  char *catch_line(char *str)
 {
-    int     i; //contador
-    char    *line; // str que vai ser devolvida
+    int     i;
+    char    *line;
 
-    i = 0; // atribuindo valor inicial ao contador
+    i = 0;
     if (!str || str[0] == '\0')
-        return (NULL); // condicinal se segurança
+        return (NULL);
     while (str[i] != '\n' && str[i] != '\0')
-        i++; // loop para saber a quantidade de char que deve ser alocado
-    line = ft_calloc(sizeof(char), i + 2); // alocando memória e garantindo espaço para \n e \0
+        i++;
+    line = ft_calloc(sizeof(char), i + 2);
     if (!line)
-        return (NULL); // condicional de segurança 
-    i = 0; // reatribuindo valor, para iniciar do 0 novamente
-    while (str[i] != '\n' && str[i] != '\0') // loop para caminhar por str
+        return (NULL);
+    i = 0;
+    while (str[i] != '\n' && str[i] != '\0')
     {
-        line[i] = str[i]; // copiando o valor de str à line
+        line[i] = str[i];
         ++i;
     }
-    if (str[i] == '\n') // condicional para add \n ao fnal de line
-        line[i++] = '\n';  // caminhando para a proxima posiçao já atribuindo \n a posiçao atual
-    line[i] = '\0'; // add \0 a posiçao final
-    return (line); // retorna a linha pronta
+    if (str[i] == '\n')
+        line[i++] = '\n';
+    line[i] = '\0';
+    return (line);
 }
 
 static char    *get_line_from_buffer(char *buffer, char *line)
@@ -33,26 +33,26 @@ static char    *get_line_from_buffer(char *buffer, char *line)
     char    *free_catch;
 
     free_catch = catch_line(buffer);
-    post_nl = ft_strchr(buffer, '\n'); // aqui pego o final, o resto do que foi lido, depois de encontrar o \n
-    if(post_nl == NULL) // se nao encontrei o \n, só sigo o loop jogando buffer em line
+    post_nl = ft_strchr(buffer, '\n');
+    if(post_nl == NULL)
     {
-        aux = line; // Ponteiro pra nao perder o line antigo
-        line = ft_strjoin(line, buffer); // atribuindo o valor do novo line  
-        free(aux); // liberando o ponteiro auxiliar       
+        aux = line;
+        line = ft_strjoin(line, buffer);
+        free(aux);
         if (!line)
             return(NULL);
     }
-    else // encontrou um \n
+    else
     {
-        len = ft_strlen(post_nl + 1); //defininho o tamanho de len
-        aux = line;  // Ponteiro pra nao perder o line antigo
-        line = ft_strjoin(line, free_catch); // junto o final da linha com line
+        len = ft_strlen(post_nl + 1);
+        aux = line;
+        line = ft_strjoin(line, free_catch);
         free(free_catch);
-        free(aux); // liberando o ponteiro auxiliar 
+        free(aux);
         if (!line)
             return(NULL);
-        ft_memmove(buffer, post_nl + 1, len); // movimento o resto lido, pra dentro buffer, definindo como o inicio da proxima vez que for ler 
-        buffer[len] = '\0'; // add o \0 ao final do buffer
+        ft_memmove(buffer, post_nl + 1, len);
+        buffer[len] = '\0';
     }
     return(line);
 
