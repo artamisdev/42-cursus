966b95a971126b8b7f4194c28d6a914692a9a0db
Tamires Braga <tamirescamposbraga@gmail.com>
2024-10-22 20:52:23 +0200

    reading a map

--- circle-2/so_long/libft/ft_putstr_fd.c
+++ circle-2/so_long/libft/ft_putstr_fd.c
@@ -6,7 +6,7 @@
 /*   By: tacampos <tacampos@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/11 23:59:39 by tacampos          #+#    #+#             */
-/*   Updated: 2024/10/22 20:00:33 by tacampos         ###   ########.fr       */
+/*   Updated: 2024/10/22 20:40:46 by tacampos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ int	ft_putstr_fd(char *s, int fd)
 	int	i;
 
 	i = 0;
+	if (s == NULL)
+		s = "(null)";
 	while (s[i])
 	{
 		ft_putchar_fd(s[i], fd);
--- circle-2/so_long/so_long.c
+++ circle-2/so_long/so_long.c
@@ -6,14 +6,26 @@
 /*   By: tacampos <tacampos@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 11:25:50 by tacampos          #+#    #+#             */
-/*   Updated: 2024/10/22 20:04:13 by tacampos         ###   ########.fr       */
+/*   Updated: 2024/10/22 20:51:50 by tacampos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft/libft.h"
 
 int main()
-{
-    ft_printf("%s", "socorro deus");
+{   
+    int     fd;
+    char    *line;
+    fd = open ("maps/map2.ber", O_RDONLY);
+    
+    line = get_next_line(fd);
+    while (line != NULL)
+    {
+        ft_printf("%s", line);
+        free(line);
+        line = get_next_line(fd);
+    }
+    close(fd);
+    
     return(0);
 }
