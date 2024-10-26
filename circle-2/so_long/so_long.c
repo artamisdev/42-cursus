d3e37dd954e9bfc46ada1caf89199e82ea2efab3
Tamires Campos Braga Rios <tacampos@c1r2s6.42madrid.com>
2024-10-26 18:00:29 +0200

    count_lines and map

--- circle-2/so_long/so_long.c
+++ circle-2/so_long/so_long.c
@@ -6,30 +6,59 @@
 /*   By: tacampos <tacampos@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 11:25:50 by tacampos          #+#    #+#             */
-/*   Updated: 2024/10/23 12:41:04 by tacampos         ###   ########.fr       */
+/*   Updated: 2024/10/26 17:58:56 by tacampos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft/libft.h"
 
-int main()
-{   
+int count_lines(void)
+{
     int     fd;
+    int     line_count;
     char    *line;
-    char    **column;
     
     fd = open ("maps/map1.ber", O_RDONLY);
-    if (fd < 0)
+     if (fd < 0)
         return(1);
-    
+        
+    line_count = 0;
     line = get_next_line(fd);
     while (line != NULL)
     {
-        ft_printf("%s", line);
+        line_count++;
         free(line);
         line = get_next_line(fd);
     }
     close(fd);
+   //ft_printf("\n%d",line_count);
+    return(line_count);
+}
+
+int main()
+{   
+    int     fd;
+    int     i;
+    size_t  line_count;
+    char    *line;
+    char    **map;
     
+    fd = open ("maps/map1.ber", O_RDONLY);
+    if (fd < 0)
+        return(1);
+    line_count = count_lines();
+    map = ft_calloc(line_count, sizeof(char*));
+    if (!map)
+        return(1);
+    i = 0;
+    line = get_next_line(fd);
+    while (line != NULL)
+    {
+        map[i] = line;
+        ft_printf("%s", line);
+        line = get_next_line(fd);
+        i++;
+    }
+    close(fd);
     return(0);
 }
