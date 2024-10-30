df43a8dea7a443504dfedb5ebdafae88f242576c
Tamires Campos Braga Rios <tacampos@c2r10s6.42madrid.com>
2024-10-30 20:44:27 +0100

    verify maps

--- circle-2/so_long/so_long.c
+++ circle-2/so_long/so_long.c
@@ -6,7 +6,7 @@
 /*   By: tacampos <tacampos@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 11:25:50 by tacampos          #+#    #+#             */
-/*   Updated: 2024/10/28 20:34:40 by tacampos         ###   ########.fr       */
+/*   Updated: 2024/10/30 20:41:53 by tacampos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,6 +35,36 @@ int count_lines(char *file_name)
     return(line_count);
 }
 
+int check_width_map(char *file_name)
+{ 
+    int     fd;
+    char    *read_line;
+    size_t  width;
+    size_t  len;
+    int     error;
+    
+    fd = open (file_name, O_RDONLY);
+    if (fd < 0)
+        return(1);
+    error = 0;
+    read_line = get_next_line(fd);
+    width = ft_strlen(read_line);
+    if (ft_strchr(read_line, '\n') != NULL)
+        width--;
+    while (read_line != NULL)
+    {
+        len = ft_strlen(read_line);
+        if (ft_strchr(read_line, '\n') != NULL)
+            len--;
+        if (len != width)
+            error = 1;
+        free(read_line);
+        read_line = get_next_line(fd);
+    }
+    close(fd);
+    return(error);
+}
+
 int main(int argc, char **argv)
 {   
     int     fd;
@@ -55,6 +85,8 @@ int main(int argc, char **argv)
     if (fd < 0)
         return(ft_printf("Could not open file!\n"));
     line_count = count_lines(argv[1]);
+    if (check_width_map(argv[1]) == 1)
+        return(ft_printf("This map is not rectangular!\n"));
     map = ft_calloc(line_count, sizeof(char*));
     if (!map)
         return(ft_printf("Failed allocating memory for map!\n"));
