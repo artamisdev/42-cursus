1b3284554d1938db3ca8963105732362dd1e9092
Tamires Braga <tamirescamposbraga@gmail.com>
2024-11-03 20:13:22 +0100

    invalid_char_map add

--- circle-2/so_long/so_long.c
+++ circle-2/so_long/so_long.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   so_long.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tacampos <tacampos@student.42.fr>          +#+  +:+       +#+        */
+/*   By: tacampos <tacampos@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 11:25:50 by tacampos          #+#    #+#             */
-/*   Updated: 2024/10/30 20:41:53 by tacampos         ###   ########.fr       */
+/*   Updated: 2024/11/03 19:57:33 by tacampos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,6 +65,39 @@ int check_width_map(char *file_name)
     return(error);
 }
 
+int check_valid_char_map(char *file_name)
+{
+    int     fd;
+    int     error;
+    size_t  j;
+    char    *str;
+    
+    fd = open (file_name, O_RDONLY);
+    if (fd < 0)
+        return(1);  
+    error = 0;
+    str = get_next_line(fd);
+    while (str != NULL)
+    {
+        j = 0;
+        while(j < ft_strlen(str))
+        {
+            if (str[j] != '1' 
+                && str[j] != '0'
+                && str[j] != 'E'
+                && str[j] != 'C'
+                && str[j] != 'P' 
+                && str[j] != '\n')
+                  error = 1;
+                j++;
+        }
+        free(str);
+        str = get_next_line(fd);
+    }
+    close(fd);
+    return(error);
+}
+
 int main(int argc, char **argv)
 {   
     int     fd;
@@ -80,13 +113,15 @@ int main(int argc, char **argv)
     confirm = ft_strrchr(argv[1], '.');
     check = ft_strncmp(confirm, ".ber", ft_strlen(confirm));
     if (check != 0)
-        return(ft_printf("bad file extension (%s)\n", confirm));    
+        return(ft_printf("bad file extension (%s)\n", confirm));   
     fd = open (argv[1], O_RDONLY);
     if (fd < 0)
         return(ft_printf("Could not open file!\n"));
     line_count = count_lines(argv[1]);
     if (check_width_map(argv[1]) == 1)
         return(ft_printf("This map is not rectangular!\n"));
+    if (check_valid_char_map(argv[1]) == 1)
+        return(ft_printf("This is a invalid map!\n"));
     map = ft_calloc(line_count, sizeof(char*));
     if (!map)
         return(ft_printf("Failed allocating memory for map!\n"));
